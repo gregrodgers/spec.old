ddc30673c1d33bcba92b64b008860d2a7ae3003f
Marty McFadden <mcfadden8@llnl.gov>
2017-09-25 14:20:05 -0400

    Changed types to macros

--- tool_support/ompd/ompd_types.h
+++ tool_support/ompd/ompd_types.h
@@ -1,62 +1,61 @@
 /*
-* @@name:	ompd_example_types.1c
-* @@type:	C
-* @@compilable:	no
-* @@linkable:	no
-* @@expect:	nothing
+* @@name:	ompd_types.h
 */
-#include "cudadebugger.h"
 #include "ompd.h"
 
 /* Kinds of device device address spaces */
-typedef enum ompd_device_kind_t {
-  ompd_device_kind_host = 1,
-  ompd_device_kind_cuda = 2
-  /* More devices... */
-} ompd_device_kind_t;
+typedef uint64_t ompd_device_kind_t;
+
+#define OMPD_DEVICE_KIND_HOST 1
+#define OMPD_DEVICE_KIND_CUDA 2
+/* More devices... */
+#define OMPD_DEVICE_RESERVED  1000000
 
 /* Kinds of device threads  */
-typedef enum ompd_osthread_kind_t {
-  ompd_osthread_pthread=0,
-  ompd_osthread_lwp=1,
-  ompd_osthread_winthread=2,
-  ompd_osthread_cudalogical=3
-} ompd_osthread_kind_t;
+typedef uint64_t ompd_osthread_kind_t;
+#define OMPD_OSTHREAD_PTHREAD 0
+#define OMPD_OSTHREAD_LWP 1
+#define OMPD_OSTHREAD_WINTHREAD 2
+#define OMPD_OSTHREAD_CUDALOGICAL 3
+/* More thread types... */
+#define OMPD_OSTHREAD_RESERVED 1000000
 
 /* Target device-specific thread identification */
 typedef struct ompd_dim3_t {
-    ompd_tword_t x;
-    ompd_tword_t y;
-    ompd_tword_t z;
+    ompd_word_t x;
+    ompd_word_t y;
+    ompd_word_t z;
 } ompd_dim3_t;
 
 typedef struct ompd_cudathread_coord_t {
-    ompd_taddr_t cudaDevId;
-    ompd_taddr_t cudaContext;
-    ompd_taddr_t warpSize;
-    ompd_taddr_t gridId;
-    ompd_taddr_t kernelId;
+    ompd_word_t cudaDevId;
+    ompd_word_t cudaContext;
+    ompd_word_t warpSize;
+    ompd_word_t gridId;
+    ompd_word_t kernelId;
     ompd_dim3_t  gridDim;
     ompd_dim3_t  blockDim;
     ompd_dim3_t  blockIdx;
     ompd_dim3_t  threadIdx;
 } ompd_cudathread_coord_t;
 
-/* Target device-specific Memory Access */
-#define OMPD_SEGMENT_CUDA_PTX_UNSPECIFIED    ((ompd_taddr_t)0)
-#define OMPD_SEGMENT_CUDA_PTX_CODE           ((ompd_taddr_t)1)
-#define OMPD_SEGMENT_CUDA_PTX_REG            ((ompd_taddr_t)2)
-#define OMPD_SEGMENT_CUDA_PTX_SREG           ((ompd_taddr_t)3)
-#define OMPD_SEGMENT_CUDA_PTX_CONST          ((ompd_taddr_t)4)
-#define OMPD_SEGMENT_CUDA_PTX_GLOBAL         ((ompd_taddr_t)5)
-#define OMPD_SEGMENT_CUDA_PTX_LOCAL          ((ompd_taddr_t)6)
-#define OMPD_SEGMENT_CUDA_PTX_PARAM          ((ompd_taddr_t)7)
-#define OMPD_SEGMENT_CUDA_PTX_SHARED         ((ompd_taddr_t)8)
-#define OMPD_SEGMENT_CUDA_PTX_SURF           ((ompd_taddr_t)9)
-#define OMPD_SEGMENT_CUDA_PTX_TEX            ((ompd_taddr_t)10)
-#define OMPD_SEGMENT_CUDA_PTX_TEXSAMPLER     ((ompd_taddr_t)11)
-#define OMPD_SEGMENT_CUDA_PTX_GENERIC        ((ompd_taddr_t)12)
-#define OMPD_SEGMENT_CUDA_PTX_IPARAM         ((ompd_taddr_t)13)
-#define OMPD_SEGMENT_CUDA_PTX_OPARAM         ((ompd_taddr_t)14)
-#define OMPD_SEGMENT_CUDA_PTX_FRAME          ((ompd_taddr_t)15)
-#define OMPD_SEGMENT_CUDA_PTX_MAX            ((ompd_taddr_t)16)
+/* Memory Access Segment definitions for Host and Target Devices */
+#define OMPD_SEGMENT_UNSPECIFIED             ((ompd_seg_t)0)
+
+#define OMPD_SEGMENT_CUDA_PTX_UNSPECIFIED    ((ompd_seg_t)0)
+#define OMPD_SEGMENT_CUDA_PTX_CODE           ((ompd_seg_t)1)
+#define OMPD_SEGMENT_CUDA_PTX_REG            ((ompd_seg_t)2)
+#define OMPD_SEGMENT_CUDA_PTX_SREG           ((ompd_seg_t)3)
+#define OMPD_SEGMENT_CUDA_PTX_CONST          ((ompd_seg_t)4)
+#define OMPD_SEGMENT_CUDA_PTX_GLOBAL         ((ompd_seg_t)5)
+#define OMPD_SEGMENT_CUDA_PTX_LOCAL          ((ompd_seg_t)6)
+#define OMPD_SEGMENT_CUDA_PTX_PARAM          ((ompd_seg_t)7)
+#define OMPD_SEGMENT_CUDA_PTX_SHARED         ((ompd_seg_t)8)
+#define OMPD_SEGMENT_CUDA_PTX_SURF           ((ompd_seg_t)9)
+#define OMPD_SEGMENT_CUDA_PTX_TEX            ((ompd_seg_t)10)
+#define OMPD_SEGMENT_CUDA_PTX_TEXSAMPLER     ((ompd_seg_t)11)
+#define OMPD_SEGMENT_CUDA_PTX_GENERIC        ((ompd_seg_t)12)
+#define OMPD_SEGMENT_CUDA_PTX_IPARAM         ((ompd_seg_t)13)
+#define OMPD_SEGMENT_CUDA_PTX_OPARAM         ((ompd_seg_t)14)
+#define OMPD_SEGMENT_CUDA_PTX_FRAME          ((ompd_seg_t)15)
+#define OMPD_SEGMENT_CUDA_PTX_MAX            ((ompd_seg_t)16)
